a74c62623686bba7395328dd3817ae70e6fa6465
samdegroot1 <samdegroot150@hotmail.com>
2017-05-07 11:53:50 +0200

    project init

---
+++ _ss_environment.php
@@ -0,0 +1,9 @@
+<?php
+
+define('SS_ENVIRONMENT_TYPE', 'dev');
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+define('SS_DATABASE_SERVER', 'localhost');
+define('SS_DATABASE_USERNAME', '');
+define('SS_DATABASE_PASSWORD', '');
