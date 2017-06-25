8b4bb7ca7938d8143e12ff95d715d4c7e95325d6
Sam de Groot <samdegroot150@hotmail.com>
2017-06-25 11:57:58 -0700

    grunt tasks juist opgezet

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,9 +7,28 @@ module.exports = function(grunt) {
         sass: {
             dist: {
                 files: {
-                    'themes/'
+                    'themes/nldarts/css/style.css' : 'themes/nldarts/css/main.scss'
                 }
             }
-        }
+        },
+        cssmin: {
+           dist: {
+              files: {
+                 'themes/nldarts/css/style.min.css': ['themes/nldarts/css/**/*.css']
+              }
+          }
+        },
+        watch: {
+            css: {
+                files: '**/*.scss',
+                tasks: ['sass', 'cssmin']
+            }
+        },
+        
     });
+    
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.registerTask('default', ['watch', 'cssmin']);
 }
